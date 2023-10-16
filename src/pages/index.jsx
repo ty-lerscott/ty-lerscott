f3a83e7f944fb207e64a6a84f6829b5211f8a8ef
Tyler Williams <ty@lerscott.com>
2023-10-16 19:13:27 -0400

    Forgot package lock

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Tyler Scott's Blog | Coming Soon</title>
+        <title>Tyler Scott&apos;s Blog | Coming Soon</title>
       </Head>
       <div className="flex justify-center content-center flex-wrap flex-col h-screen v-screen">
-        <h1 className="text-center">Tyler Scott's Blog</h1>
+        <h1 className="text-center">Tyler Scott&apos;s Blog</h1>
         <p className="text-center">Coming Soon</p>
       </div>
     </>
