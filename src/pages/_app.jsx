b9a270aa95ddf3691a18a6feeba1b9921227c310
Tyler Williams <ty@lerscott.com>
2023-10-17 18:57:45 -0400

    Basic Typography

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,33 @@
 import "@/app/styles/globals.css";
 
+import { Open_Sans, Montserrat } from "next/font/google";
+
+export const OpenSansFont = Open_Sans({
+  subsets: ["latin"],
+  variable: "--font-open-sans",
+});
+
+export const MontserratFont = Montserrat({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={OpenSansFont.className}>
+      <style jsx global>{`
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+          font-family: ${MontserratFont.style.fontFamily};
+          font-weight: 700;
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </main>
+  );
 }
