3f5d15dd633229e20638e6054539993d85af1270
Tyler Scott <ty@lerscott.com>
2024-07-07 01:15:41 -0400

    test playwright

--- src/app/api/download/resume/route.ts
+++ src/app/api/download/resume/route.ts
@@ -1,60 +1,41 @@
 import pkg from "~/package.json";
-import puppeteerLocal from "puppeteer";
-import puppeteer from "puppeteer-core";
-import chromium from "@sparticuz/chromium";
+import { chromium } from "playwright";
 
 const isLocal = process.env.NODE_ENV !== "production";
 
+const PDF_OPTIONS = {
+  format: "A4",
+  margin: {
+    top: "1cm",
+    left: "1cm",
+    right: "1cm",
+    bottom: "1cm",
+  },
+};
+
 export async function GET() {
   try {
-    // Launch a new browser instance
-    let browser;
-    if (process.env.NODE_ENV === "development") {
-      // For local development, use the installed Chrome
-      browser = await puppeteerLocal.launch();
-    } else {
-      // For production (Vercel)
-      browser = await puppeteer.launch({
-        args: chromium.args,
-        defaultViewport: chromium.defaultViewport,
-        executablePath: await chromium.executablePath(),
-        headless: chromium.headless,
-      });
-    }
-    const page = await browser.newPage();
-
-    const url = isLocal
+    const pathname = isLocal
       ? "http://ty.lerscott.local:3000"
       : "https://ty.lerscott.com";
+    const url = `${pathname}/resume/simple`;
 
-    // Navigate to the desired page
-    await page.goto(`${url}/resume/simple`, { waitUntil: "networkidle0" });
-
-    // Generate PDF
-    const pdf = await page.pdf({
-      format: "A4",
-      margin: {
-        top: "1cm",
-        left: "1cm",
-        right: "1cm",
-        bottom: "1cm",
-      },
-    });
-
-    // Close the browser
-    await browser.close();
+    const browser = await chromium.launch(); // Or 'firefox' or 'webkit'.
+    const page = await browser.newPage();
+    await page.emulateMedia({ media: "screen" });
+    await page.goto(url);
+    const pdfBuffer = await page.pdf(PDF_OPTIONS);
 
     const fileName = pkg.author.name.toLowerCase().replace(/\s/g, "_");
-
     const headers = new Headers();
     headers.set("Content-Type", "application/pdf");
     headers.set(
       "Content-Disposition",
-      `attachment; filename="${fileName}_resume.pdf"`,
+      `attachment; filename=${fileName}_resume.pdf`,
     );
 
     // Return the response with PDF data
-    return new Response(pdf, {
+    return new Response(pdfBuffer, {
       headers: headers,
     });
   } catch (error) {
