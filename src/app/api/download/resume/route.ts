5d1f7dea786bf0829bbbe5522ce87535fc0173d1
Tyler Scott <ty@lerscott.com>
2024-07-07 00:46:51 -0400

    test

--- src/app/api/download/resume/route.ts
+++ src/app/api/download/resume/route.ts
@@ -1,12 +1,26 @@
 import pkg from "~/package.json";
-import puppeteer from "puppeteer";
+import puppeteerLocal from "puppeteer";
+import puppeteer from "puppeteer-core";
+import chromium from "@sparticuz/chromium";
 
 const isLocal = process.env.NODE_ENV !== "production";
 
 export async function GET() {
   try {
     // Launch a new browser instance
-    const browser = await puppeteer.launch();
+    let browser;
+    if (process.env.NODE_ENV === "development") {
+      // For local development, use the installed Chrome
+      browser = await puppeteerLocal.launch();
+    } else {
+      // For production (Vercel)
+      browser = await puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath(),
+        headless: chromium.headless,
+      });
+    }
     const page = await browser.newPage();
 
     const url = isLocal
