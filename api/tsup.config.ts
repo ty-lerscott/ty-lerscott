37758ac34794e2589da92eb5efa6b645e67f5a8b
Tyler Scott <ty@lerscott.com>
2024-12-31 17:42:58 -0500

    .

--- api/tsup.config.ts
+++ api/tsup.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "tsup";
 import env from "./src/lib/dotenv";
 
+console.log("This is tsup", env);
+
 export default defineConfig({
 	entry: ["./src/**/*.ts"],
 	outDir: "./dist",
