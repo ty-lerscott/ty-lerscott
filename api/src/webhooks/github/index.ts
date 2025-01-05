5004d6dd97a1a7565a0e135b7f8c78cf93a64c14
Tyler Scott <ty@lerscott.com>
2025-01-05 10:53:43 -0500

    add secret endpoint protection

--- api/src/webhooks/github/index.ts
+++ api/src/webhooks/github/index.ts
@@ -1,12 +1,30 @@
+import env from "@/lib/dotenv";
+import crypto from "node:crypto";
 import type { Conductor } from "@/types";
 import StatusCodes from "@/lib/status-codes";
 import DeploymentStatusController from "./deployment-status";
+
+const verifyWebhookSignature = (payload: string, signature: string) =>
+	crypto.timingSafeEqual(
+		Buffer.from(signature),
+		Buffer.from(
+			`sha1=${crypto
+				.createHmac("sha1", env.GH_WEBHOOK_SECRET)
+				.update(JSON.stringify(payload))
+				.digest("hex")}`,
+		),
+	);
 
 const GithubController = async ({ req, res }: Conductor) => {
 	const { body, method } = req;
 
 	if (method !== "POST") {
 		res.status(StatusCodes.METHOD_NOT_ALLOWED).end();
+		return;
+	}
+
+	if (!verifyWebhookSignature(req.body, req.get("x-hub-signature") as string)) {
+		res.status(StatusCodes.UNAUTHORIZED).end();
 		return;
 	}
 
