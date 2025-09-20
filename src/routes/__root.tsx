7f52da4b7564106989e030b9173a362c913a3b25
Tyler Scott <ty@lerscott.com>
2025-09-20 12:58:21 -0400

    .

---
+++ src/routes/__root.tsx
@@ -0,0 +1,20 @@
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+
+const RootLayout = () => (
+  <>
+    <div className="p-2 flex gap-2">
+      <Link to="/">
+        Home
+      </Link>{' '}
+      <Link to="/about">
+        About
+      </Link>
+    </div>
+    <hr />
+    <Outlet />
+    <TanStackRouterDevtools />
+  </>
+)
+
+export const Route = createRootRoute({ component: RootLayout })
